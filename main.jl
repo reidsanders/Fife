e25aad5d981ee9df8b4c024def7931d3d29a141f
Reid Sanders <reid@reidsanders.net>
2020-08-20 20:02:02 -0400

    added epsilon to norm to fix nans,
    renamed to normit

--- main.jl
+++ main.jl
@@ -2,6 +2,7 @@ using Pkg
 Pkg.activate(".")
 using Flux: onehot, onehotbatch, onecold, crossentropy, logitcrossentropy, glorot_uniform, mse, epseltype
 using Flux
+using Flux.Optimise: update!
 using CUDA
 using Zygote
 using Random
@@ -12,40 +13,29 @@ CUDA.allowscalar(false)
 # TODO use GPU / Torch tensors for better performance
 # TODO use threads (if running program on cpu at least)
 struct VMState
-    # instruction pointer (prob vector)
     current_instruction::Vector{Float32}
-    ## top of stack pointer (prob vector)
     top_of_stack::Vector{Float32}
-    ## data stack (stacked prob vectors for each possible datum.)
     stack::Array{Float32}
 end
+Zygote.@adjoint VMState(x,y,z) = VMState(x,y,z), di -> (di.current_instruction, di.top_of_stack, di.stack)
+a::Number * b::VMState = VMState(a * b.current_instruction, a * b.top_of_stack, a * b.stack)
+a::VMState * b::Number = b * a
+a::VMState + b::VMState = VMState(a.current_instruction + b.current_instruction, a.top_of_stack + b.top_of_stack, a.stack + b.stack)
+a::VMState - b::VMState = VMState(a.current_instruction - b.current_instruction, a.top_of_stack - b.top_of_stack, a.stack - b.stack)
 
 function super_step(state::VMState, program, instructions)
     new_states = [instruction(state) for instruction in instructions]
-    # instruction_probs = sum(program .* state.current_instruction',dims=2) 
-    # scaled_states = instruction_probs .* new_states
-    # summed = reduce(+, scaled_states)
-    # normed = norm(summed)
-    norm(reduce(+, sum(program .* state.current_instruction',dims=2) .* new_states))
+    normit(reduce(+, sum(program .* state.current_instruction',dims=2) .* new_states))
 end
 
 function instr_dup(state::VMState)
     #=
     DUP Should duplicate top of stack, and push to top of stack
-
-    move data stack pointer vector up by 1 ?
-    mult each row by corresponding  data stack vector entry (representing prob)
-
-    prob of word * (Data_Stack array * Top of Stack pointer vector) + (Data_Stack shifted down 1)
-
-    W * D * SP + D~1
-
-    Shift stack Pointer?
     =#
-    # TODO Roll first
     new_top_of_stack = roll(state.top_of_stack,-1)
     new_stack = state.stack .* (1.0 .- state.top_of_stack') .+ state.stack .* new_top_of_stack'
-    new_current_instruction = roll(state.current_instruction,-1)
+    new_current_instruction = roll(state.current_instruction,1)
+
     VMState(
         new_current_instruction,
         new_top_of_stack,
@@ -54,14 +44,8 @@ function instr_dup(state::VMState)
     
 end
 
-#def swap(stack):
-    #"""
-    #SWAP Swap top two elements in stack
-
-    #prob_of_word * (Data_Stack * Top of Stack )
-
-    #"""
-
+# TODO normit after most instr (?)
+# TODO def normit for  all zero case
 
 instr_pass(state::VMState) = state
 instr_0(state::VMState) = instr_val(state,0,allvalues) # TODO create lamdas for all
@@ -71,25 +55,7 @@ instr_3(state::VMState) = instr_val(state,3,allvalues) # TODO create lamdas for 
 instr_4(state::VMState) = instr_val(state,4,allvalues) # TODO create lamdas for all
 instr_5(state::VMState) = instr_val(state,5,allvalues) # TODO create lamdas for all
 
-function roll!(a::Vector, increment)
-    # Only vectors right now
-    # use hcat / vcat otherwise?
-    if increment < 0
-        for i in 1:-increment
-            push!(a, a[1])
-            popfirst!(a)
-        end
-    else
-        for i in 1:increment
-            pushfirst!(a, a[end])
-            pop!(a)
-        end
-    end 
-end
-
 function roll(a::Vector, increment)
-    # Only vectors right now
-    # use hcat / vcat otherwise?
     increment = increment%length(a)
     if increment == 0
         return a
@@ -99,41 +65,24 @@ function roll(a::Vector, increment)
         return vcat(a[end+increment-1:end], a[1:end-increment])
     end 
 end
-
-# immutable ImArray <: AbstractArray
-#     data::
-
 function instr_val(state::VMState, val, allvalues)
     # This seems really inefficient...
     # Preallocate intermediate arrays? 1 intermediate state for each possible command, so not bad to allocate ahead of time
     # sizehint
     # set return type to force allocation
 
-    # new_stack = similar(state.stack)
-    # valhot = convert(Vector{Float32},onehot(val, allvalues))
     valhot = [i == val ? 1.0f0 : 0.0f0 for i in allvalues]
-    # for (i, col) in enumerate(eachcol(state.stack))
-    #     new_stack[:,i] = (0.0-state.top_of_stack[i]) .* col .+ (state.top_of_stack[i] .* valhot)
-    # end
     new_top_of_stack = roll(state.top_of_stack,-1)
-    new_current_instruction = roll(state.current_instruction,-1)
+    new_current_instruction = roll(state.current_instruction,1)
     new_stack = state.stack .* (1.0 .- new_top_of_stack') .+ valhot * new_top_of_stack'
-
-    # new_stack = 1.0 .- state.stack
-
-    # print(new_stack)
-
-    new_state = VMState(
+    VMState(
         new_current_instruction,
         new_top_of_stack,
         new_stack,
     )
-    # check_state_asserts(new_state)
-    new_state
 end
 
 function check_state_asserts(state)
-
     @assert sum(state.current_instruction) == 1.0
     @assert sum(state.top_of_stack) == 1.0
     for col in eachcol(state.stack)
@@ -145,39 +94,23 @@ function create_random_discrete_program(len, instructions)
     program = [rand(instructions) for i in 1:len]
 end
 
-# Set trainable parts of program.
 function create_trainable_mask(program_len, input_len)
     mask = falses(program_len)
     mask[input_len+1:end] .= true
     mask
 end
 
-function create_example_batch(batch_size, program_len, input_len)
-    # Create input, output
-    #
-    for i in 1:batch_size
-        create_random_program
-    end
-end
-
 # TODO terminate all program in Null operator? Early stopping if that last instruction is large percentage?
 function run(state, program, instructions, ticks)
-    # TODO run (takes program superposition and runs it. returns state?)
     for i in 1:ticks
         state = super_step(state, program, instructions)
-        # @assert !any(isnan.(program)) ## Damn, putting an assert removes the NaN
-        @assert !any(isnan.(state.stack)) ## Damn, putting an assert removes the NaN
-        @assert !any(isnan.(state.top_of_stack)) ## Damn, putting an assert removes the NaN
-        @assert !any(isnan.(state.current_instruction)) ## Damn, putting an assert removes the NaN
-        @assert !any(isnan.(program)) ## Damn, putting an assert removes the NaN
+        assert_no_nans(state)
     end
     state
 end
 
 function loss(ŷ, y)
-    # TODO loss (takes y ŷ  stacks (?) and )
-    # use logitcrossentropy ? 
-    crossentropy(ŷ, y, ϵ=2*epseltype(ŷ))
+    crossentropy(ŷ, y)
 end
 
 function init_state(data_stack_depth, program_len, allvalues)
@@ -191,31 +124,36 @@ function init_state(data_stack_depth, program_len, allvalues)
         stack,
     )
     state.current_instruction[1] = 1.f0
-    # state.top_of_stack[fld(data_stack_depth,2)] = 1.f0
     state.top_of_stack[1] = 1.f0
     state
 end
+
+function softmaxmask(mask, prog)
+    new = softmax(prog) .* mask' + prog .* (.~ mask)' 
+end
+function partial(f,a...)
+    ( (b...) -> f(a...,b...) )
+end
+
+function normit(a::Array; dims=1, ϵ=epseltype(a))
+    new = a .+ ϵ
+    new ./ sum(new, dims=dims) 
+    # TODO if sum to 0, 
+    # new = relu.(a)
+    # new = new ./ sum(new, dims=dims)
+end
+
+function normit(a::VMState; dims=1)
+    VMState(
+        normit(a.current_instruction, dims=dims),
+        normit(a.top_of_stack, dims=dims),
+        normit(a.stack, dims=dims),
+    )
+
+end
 function main()
-    # current_instruction = zeros(Float32, program_len,)::Vector{Float32}
-    # stack = CuArray(zeros(length(allvalues), data_stack_depth))
-    # current_instruction = CuArray(zeros(Float32,program_len,))
-    # top_of_stack = CuArray(zeros(Float32,data_stack_depth,))
-
-    # stack = zeros(length(allvalues), data_stack_depth)
-    # current_instruction = zeros(Float32,program_len,)
-    # top_of_stack = zeros(Float32,data_stack_depth,)
-    # stack[1,:] .= 1.f0
-    # state = VMState(
-    #     current_instruction,
-    #     top_of_stack,
-    #     stack,
-    # )
-    # state.current_instruction[1] = 1.f0
-    # state.top_of_stack[fld(data_stack_depth,2)] = 1.f0
-
     state = init_state(data_stack_depth, program_len)
     state = run(state, program, instructions, max_ticks)
-
     collapsed_program = onecold(program)
 end
 
@@ -240,33 +178,48 @@ function custom_train!(loss, ps, data, opt)
     end
   end
 
-# Change target_program untrainable part
 function get_program_with_random_inputs(program, mask)
-    # Assumes columns are onehot
     num_instructions = size(program)[1]
     new_program = copy(program)
     for (i,col) in enumerate(eachcol(new_program[:, mask]))
         new_program[:,i] .= false
         new_program[rand(1:num_instructions),i] = true
-        # col = rand(size(col))
     end
     new_program
 end
 
 function create_program_batch(startprogram, train_mask, batch_size)
-    program_batch = 
     target_program = program()
     for i in 1:batch_size
         program
     end
 end
 
+function assert_no_nans(state::VMState)
+    @assert !any(isnan.(state.stack)) ## Damn, putting an assert removes the NaN
+    @assert !any(isnan.(state.top_of_stack)) ## Damn, putting an assert removes the NaN
+    @assert !any(isnan.(state.current_instruction)) ## Damn, putting an assert removes the NaN
+end
+function forward(state, hiddenprogram, target, instructions, program_len)
+    program = softmaxprog(hiddenprogram)
+    pred = run(state, program, instructions, program_len)
+    loss(pred.stack, target.stack)
+end
+
+function trainloop()
+    for i in 1:10
+        display(i)
+        display(hiddenprogram)
+        update!(opt, trainable, gradprog(hiddenprogram)[:, train_mask])
+    end
+end
+
 data_stack_depth = 20
 program_len = 10
-input_len = 4 # frozen
+input_len = 4 # frozen part
 max_ticks = 4
-# instructions = [instr_0, instr_1, instr_2, instr_3, instr_4, instr_5, instr_dup]
-instructions = [instr_0, instr_1, instr_2, instr_3, instr_4, instr_5]
+instructions = [instr_0, instr_1, instr_2, instr_3, instr_4, instr_5, instr_dup]
+# instructions = [instr_0, instr_1, instr_2, instr_3, instr_4, instr_5]
 # instructions = [instr_3, instr_4, instr_5]
 num_instructions = length(instructions)
 allvalues = [["blank"]; [i for i in 0:5]]
@@ -279,108 +232,32 @@ hiddenprogram = deepcopy(target_program)
 train_mask = create_trainable_mask(program_len, input_len)
 
 #Initialize
-function softmaxmask(mask, prog)
-    new = softmax(prog) .* mask' + prog .* (.~ mask)' 
-end
-function partial(f,a...)
-    ( (b...) -> f(a...,b...) )
-end
-
-function norm(a::Array; dims=1)
-    new = relu.(a)
-    new = new ./ sum(new, dims=dims)
-end
-
-function norm(a::VMState; dims=1)
-    VMState(
-        norm(a.current_instruction, dims=dims),
-        norm(a.top_of_stack, dims=dims),
-        norm(a.stack, dims=dims),
-    )
-
-end
 softmaxprog = partial(softmaxmask, train_mask)
 
 hiddenprogram[:, train_mask] = glorot_uniform(size(hiddenprogram[:, train_mask]))
 program = softmaxprog(hiddenprogram)
-# program[:, train_mask] = softmax(glorot_uniform(size(program[:, train_mask])))
-# program[:, train_mask] = softmax(rand(size(program[:, train_mask])))
 blank_state = init_state(data_stack_depth, program_len, allvalues)
 # TODO Do we want it to be possible to move instruction pointer to "before" the input?
-
-
-# ps = params(@views program[:, train_mask])
-
-
 
 # newtrainprogram = get_program_with_random_inputs(program, .~train_mask)
 # newtargetprogram = copy(target_program)
 # newtargetprogram[:, .~train_mask] = newtrainprogram[:, .~train_mask]
 
-Zygote.@adjoint VMState(x,y,z) = VMState(x,y,z), di -> (di.current_instruction, di.top_of_stack, di.stack)
-a::Number * b::VMState = VMState(a * b.current_instruction, a * b.top_of_stack, a * b.stack)
-a::VMState * b::Number = b * a
-a::VMState + b::VMState = VMState(a.current_instruction + b.current_instruction, a.top_of_stack + b.top_of_stack, a.stack + b.stack)
-a::VMState - b::VMState = VMState(a.current_instruction - b.current_instruction, a.top_of_stack - b.top_of_stack, a.stack - b.stack)
-
-# sumloss(state) = sum(instr_5(state).stack)
-# gradient(sumloss, blank_state)
-
-# sumloss(state) = sum(super_step(state,program,instructions).stack)
-# gradient(sumloss, blank_state)
 
 target = run(blank_state, target_program, instructions, program_len)
 prediction = run(blank_state, program, instructions, program_len)
-# first_loss = crossentropy(prediction.stack, target.stack)
 first_loss = loss(prediction.stack, target.stack)
-# first_loss = mse(prediction.stack, target.stack)
+@show first_loss
 
-# # trainable = @views program[:, train_mask]
-# ps = params(program)
-# # ps = params(trainable)
-
-# gs = gradient(ps) do 
-#     # target = run(blank_state, target_program, instructions, program_len)
-#     pred = run(blank_state, program, instructions, program_len)
-#     # logitcrossentropy(pred.stack, target.stack)
-#     crossentropy(pred.stack, target.stack)
-#     # mse(pred.stack, target.stack)
-# end
-# gs[program]
-
-using Flux.Optimise: update!
-
-
-function forward(state, hiddenprogram, target, instructions, program_len)
-    program = softmaxprog(hiddenprogram)
-    # program = softmaxmask(train_mask, hiddenprogram)
-    pred = run(state, program, instructions, program_len)
-    # mse(pred.stack, target.stack)
-    # crossentropy(pred.stack, target.stack)
-    # logitcrossentropy(pred.stack, target.stack)
-    loss(pred.stack, target.stack)
-end
 
 gradprog(hidden) = gradient(forward,blank_state,hidden,target,instructions,program_len)[2]
-gradprog(hiddenprogram)
 
 first_program = deepcopy(program)
 # opt = Descent(0.05) # Gradient descent with learning rate 0.1
 opt = ADAM(0.001) # Gradient descent with learning rate 0.1
 trainable = @views hiddenprogram[:,train_mask]
 
-
-@show first_loss
-function trainloop()
-    for i in 1:1000
-        display(i)
-        display(hiddenprogram)
-        update!(opt, trainable, gradprog(hiddenprogram)[:, train_mask])
-    end
-end
-# @enter trainloop()
 trainloop()
-
 runprog(prog) = run(blank_state, prog, instructions, program_len)
 
 program = softmaxprog(hiddenprogram)
@@ -397,17 +274,3 @@ display(program)
 # why is gradient sign neg for both instructions in program (for crossentrop)
 # why do both losses have a gradient for first instruction (which is exactly accurate so should be 0!)
 # TODO mult by top_of_stack before loss (it is relevant afterall)
-
-
-# ps = params(program)
-# gs2 = gradient(ps) do 
-#     pred = run(blank_state, program, instructions, program_len)
-#     crossentropy(pred.stack, target.stack)
-# end
-# gs2[program]
-
-
-# Switch to straight normed to 1 vs softmax? or keep program as a separate unconstrained value, and apply softmax before pushing it into run? Then use logitcrossentropy
-# Eg add hidden program with unconstrained values
-# sumloss(state) = sum(super_step(state,program,instructions).stack)
-# gradient(sumloss, blank_state)
