7cbe1f20b8171f1100cc6e2d050f34efd4b2046a
Reid Sanders <reid@reidsanders.net>
2020-08-27 23:47:52 -0400

    maybe working goto

--- main.jl
+++ main.jl
@@ -22,12 +22,6 @@ struct VMState
     current_instruction::Union{Array{Float32},CuArray{Float32}}
     top_of_stack::Union{Array{Float32},CuArray{Float32}}
     stack::Union{Array{Float32},CuArray{Float32}}
-    # current_instruction::Array{Float32}
-    # top_of_stack::Array{Float32}
-    # stack::Array{Float32}
-    # current_instruction::CuArray{Float32}
-    # top_of_stack::CuArray{Float32}
-    # stack::CuArray{Float32}
 end
 
 struct VMSuperStates
@@ -48,25 +42,10 @@ a::Union{Array,CuArray} * b::VMSuperStates = VMSuperStates(a .* b.current_instru
 a::VMSuperStates * b::Union{Array,CuArray} = b * a
 
 function super_step(state::VMState, program, instructions)
-    # new_states = StructArray([instruction(state) for instruction in instructions])
     # TODO instead of taking a state, take the separate arrays as args? Since CuArray doesn't like Structs
-    # TODO try named tuple instead of structs?
     # TODO batch the individual array (eg add superpose dimension -- can that be a struct or needs to be separate?)
     newstates = [instruction(state) for instruction in instructions]
-    # TODO Buffer ?  dropgrad?
-    # 
-    # display(newstates)
-    # display(newstates[1].stack)
-    # display(newstates[1].top_of_stack)
-    # display(newstates[1].current_instruction)
-    # display(length(newstates))
     current_instructions = cat([x.current_instruction for x in newstates]..., dims=3)
-
-    # tmp = cat([x.top_of_stack for x in newstates]..., dims=3)
-    # top_of_stacks = similar(current_instructions)
-    # Zygote.ignore() do
-    #     top_of_stacks = cat([Zygote.dropgrad(x.top_of_stack) for x in newstates]..., dims=3)
-    # end
     top_of_stacks = cat([x.top_of_stack for x in newstates]..., dims=3)
 
     stacks = cat([x.stack for x in newstates]..., dims=3)
@@ -76,34 +55,10 @@ function super_step(state::VMState, program, instructions)
         top_of_stacks,
         stacks,
     )
-    # states = VMSuperStates(
-    #     cat([x.current_instruction for x in newstates]..., dims=3),
-    #     cat([x.top_of_stack for x in newstates]..., dims=3),
-    #     cat([x.stack for x in newstates]..., dims=3),
-    # )
-    # Split out states into array of stacks, etc here? Or define vectorized instructions application
-
-    # Check performance, but easiest to just send to cpu here
-    # display(program)
-    # display(state.current_instruction)
     current = program .* state.current_instruction'
     summed = sum(current, dims=2) 
     summed = reshape(summed,(1,1,:))
-    # display(typeof(states))
-    # summed = summed |> cpu
-    # display(states)
-    # display(summed)
-    # display(new_states)
-    # scaledstates = similar(new_states)
-    # @avx for i in 1:length(summed)
-    #     scaledstates[i] = summed[i] * new_states[i]
-    # end
-    # @assert isbits(summed)
-    # @assert isbits(new_states)
     scaledstates = summed * states 
-    # display(scaledstates)
-
-    # reduced = reduce(+, scaledstates)
     reduced = VMState( 
         sum(scaledstates.current_instructions, dims=3)[:,:,1],
         sum(scaledstates.top_of_stacks, dims=3)[:,:,1],
@@ -125,6 +80,50 @@ function instr_dup(state::VMState)
         new_current_instruction,
         new_top_of_stack,
         new_stack,
+    )
+    
+end
+
+function instr_gotoifnotzero(state::VMState)
+    #=
+    GOTO takes top two elements of stack. If top is not zero, goto second element (or end, if greater than program len)
+
+    take stack. apply bitmask for 0 / mult by zero onehot (or select col by index, but slow on gpu)
+    1.- , then mult as prob vector. 
+    Apply top of stack prob?
+    
+    =#
+
+    # new_top_of_stack = roll(state.top_of_stack,2)
+    # new_stack = state.stack .* (1.f0 .- state.top_of_stack') .+ state.stack .* new_top_of_stack'
+    # new_current_instruction = roll(state.current_instruction,1)
+
+    zerovec = valhot(0, allvalues) # TODO don't recalc 
+    stackscaled = state.stack .* state.top_of_stack'
+    probofgoto = 1 .- sum(stackscaled .* zerovec, dims=1)
+    firstpoptop = roll(state.top_of_stack,1)
+    secondstackscaled = state.stack .* firstpoptop' 
+    jumpvalprobs = sum(secondstackscaled, dims=2)
+
+    # Assume ints start at 0 should skip 0?
+    # Constraint -- length(intvalues) > length of current instruction ?
+    jumpvalprobs = jumpvalprobs[length(nonintvalues)+1:end]
+    # TODO length(current_instruction) compare and truncate
+    jumpvalprobs = jumpvalprobs[1:end]
+    currentinstructionforward = (1.f0 - sum(jumpvalprobs)) * roll(state.current_instruction,1)
+    new_current_instruction = currentinstructionforward .+ jumpvalprobs[1:length(state.current_instruction)]
+
+    # jumpvalprobs[:end]
+    # TODO set blank / zero to zero? zero goes to first? greater than length(program) goes to end?
+    # for each value 0-max in stack, 
+    # (??sum diagonal of equivalent jump locations. eg 0 at x is equivalent to 1 at x+1 on stack)
+    # TODO if not int value, set to roll 1 forward?
+    # TODO add jumpvalprobs to current_instruction? Then normalize?
+
+    VMState(
+        state.current_instruction,
+        firstpoptop,
+        state.stack,
     )
     
 end
@@ -342,8 +341,8 @@ function applyfullmask(mask,prog)
 end
 applyfullmaskprog(program) = applyfullmask(trainmaskfull, program)
 
-# use_cuda = false
-use_cuda = true
+use_cuda = false
+# use_cuda = true
 if use_cuda
     device = gpu
     # @info "Training on GPU"
@@ -353,15 +352,19 @@ else
 end
 
 
-data_stack_depth = 4000
-program_len = 100
-input_len = 50 # frozen part
-max_ticks = 100
+data_stack_depth = 5
+program_len = 2
+input_len = 1 # frozen part
+max_ticks = 10
 instructions = [instr_0, instr_1, instr_2, instr_3, instr_4, instr_5, instr_dup]
 # instructions = [instr_0, instr_1, instr_2, instr_3, instr_4, instr_5]
 # instructions = [instr_3, instr_4, instr_5]
 num_instructions = length(instructions)
-allvalues = [["blank"]; [i for i in 0:5]]
+maxint = 5
+intvalues = [i for i in 0:maxint]
+nonintvalues = ["blank"]
+allvalues = [nonintvalues; intvalues]
+
 
 
 discrete_program = create_random_discrete_program(program_len, instructions)
@@ -390,86 +393,78 @@ train_mask = train_mask |> device
 blank_state = init_state(data_stack_depth, program_len, allvalues)
 # TODO Do we want it to be possible to move instruction pointer to "before" the input?
 
-# newtrainprogram = get_program_with_random_inputs(program, .~train_mask)
+# newtrainprogram = get_program_with_random_inputs(program, .!train_mask)
 # newtargetprogram = copy(target_program)
-# newtargetprogram[:, .~train_mask] = newtrainprogram[:, .~train_mask]
+# newtargetprogram[:, .!train_mask] = newtrainprogram[:, .!train_mask]
 
-tmp = instr_3(blank_state)
-
-target = run(blank_state, target_program, instructions, program_len)
-prediction = run(blank_state, program, instructions, program_len)
-first_loss = loss(prediction.stack, target.stack)
-@show first_loss
+tmp = instr_gotoifnotzero(blank_state)
 
 
+# target = run(blank_state, target_program, instructions, program_len)
+# prediction = run(blank_state, program, instructions, program_len)
+# first_loss = loss(prediction.stack, target.stack)
+# @show first_loss
+# gradprog(hidden) = gradient(forward,blank_state,hidden,target,instructions,program_len)[2]
 
-
-
-
-
-
-
-gradprog(hidden) = gradient(forward,blank_state,hidden,target,instructions,program_len)[2]
-
-first_program = deepcopy(program)
-# opt = Descent(0.05) # Gradient descent with learning rate 0.1
-opt = ADAM(0.001) # Gradient descent with learning rate 0.1
-trainable = @views hiddenprogram[:,train_mask]
+# first_program = deepcopy(program)
+# # opt = Descent(0.05) # Gradient descent with learning rate 0.1
+# opt = ADAM(0.001) # Gradient descent with learning rate 0.1
+# trainable = @views hiddenprogram[:,train_mask]
 
 
 
-ps = Params(trainable)
+# ps = Params(trainable)
 
-gs = gradient(ps) do 
-    forward(blank_state,hiddenprogram,target,instructions,program_len)
-end
-function trainloopps(numexamples)
-    for i in 1:numexamples
-        # display(i)
-        # display(hiddenprogram)
-        gs = gradient(ps) do 
-            forward(blank_state,hiddenprogram,target,instructions,program_len)
-        end
-        # gs[program]
-        # display(hiddenprogram)
-        # update!(opt, hiddenprogram, gradprog(hiddenprogram))
-        # update!(opt, trainable, gradprog(hiddenprogram)[:, train_mask])
-        update!(opt, ps, gs)
-    end
-end
-
-
-# trainloopps(100)
-@btime trainloop(100)
+# gs = gradient(ps) do 
+#     forward(blank_state,hiddenprogram,target,instructions,program_len)
+# end
+# function trainloopps(numexamples)
+#     for i in 1:numexamples
+#         # display(i)
+#         # display(hiddenprogram)
+#         gs = gradient(ps) do 
+#             forward(blank_state,hiddenprogram,target,instructions,program_len)
+#         end
+#         # gs[program]
+#         # display(hiddenprogram)
+#         # update!(opt, hiddenprogram, gradprog(hiddenprogram))
+#         # update!(opt, trainable, gradprog(hiddenprogram)[:, train_mask])
+#         update!(opt, ps, gs)
+#     end
+# end
 
 
+# # trainloopps(100)
+# @btime trainloop(10)
 
-program = softmaxprog(hiddenprogram)
-prediction2 = run(blank_state, program, instructions, program_len)
-second_loss = loss(prediction2.stack, target.stack)
-# display(target_program)
-# display(first_program)
-# display(program)
-# @show second_loss
-@show second_loss - first_loss
 
-# predictionbatch = runprog.([program, program, program])
-#TODO why is crossentropy increasing loss
-# why is gradient sign neg for both instructions in program (for crossentrop)
-# why do both losses have a gradient for first instruction (which is exactly accurate so should be 0!)
-# TODO mult by top_of_stack before loss (it is relevant afterall)
 
-# TODO top_of_stack isnt used in gradient, so it gets iterate nothing?
-#  ignore(), use Params, dropgrad ? calc loss with current_instruction and top_of_stack?
+# program = softmaxprog(hiddenprogram)
+# prediction2 = run(blank_state, program, instructions, program_len)
+# second_loss = loss(prediction2.stack, target.stack)
+# # display(target_program)
+# # display(first_program)
+# # display(program)
+# # @show second_loss
+# @show second_loss - first_loss
 
-function compare_programs(hidden, target, trainmaskfull)
-    # prog = softmaxprog(hidden)[trainmaskfull]
-    # # display(onecold(prog))
-    # samemax = onecold(prog) .== onecold(target[trainmaskfull])
-    # display(samemax)
-    # sum(samemax)
-    (sum(onecold(hiddenprogram) .== onecold(target_program))-sum(1 .- train_mask))/sum(train_mask)
-end
-x = compare_programs(hiddenprogram, target_program, trainmaskfull)
-runprog(prog) = run(blank_state, prog, instructions, program_len)
-x
+# # predictionbatch = runprog.([program, program, program])
+# #TODO why is crossentropy increasing loss
+# # why is gradient sign neg for both instructions in program (for crossentrop)
+# # why do both losses have a gradient for first instruction (which is exactly accurate so should be 0!)
+# # TODO mult by top_of_stack before loss (it is relevant afterall)
+
+# # TODO top_of_stack isnt used in gradient, so it gets iterate nothing?
+# #  ignore(), use Params, dropgrad ? calc loss with current_instruction and top_of_stack?
+
+# function compare_programs(hidden, target, trainmaskfull)
+#     # prog = softmaxprog(hidden)[trainmaskfull]
+#     # # display(onecold(prog))
+#     # samemax = onecold(prog) .== onecold(target[trainmaskfull])
+#     # display(samemax)
+#     # sum(samemax)
+#     (sum(onecold(hiddenprogram) .== onecold(target_program))-sum(1 .- train_mask))/sum(train_mask)
+# end
+# x = compare_programs(hiddenprogram, target_program, trainmaskfull)
+# runprog(prog) = run(blank_state, prog, instructions, program_len)
+# x
