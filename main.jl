b1d086de3dbccc6570b6bdda54a64beb69f34ea7
Reid Sanders <reid@reidsanders.net>
2021-01-06 17:01:13 -0500

    renamed current_instruction to instructionpointer etc

--- main.jl
+++ main.jl
@@ -25,51 +25,51 @@ function partial(f, a...)
 end
 
 struct VMState
-    current_instruction::Union{Array{Float32},CuArray{Float32}}
-    top_of_stack::Union{Array{Float32},CuArray{Float32}}
+    instructionpointer::Union{Array{Float32},CuArray{Float32}}
+    stackpointer::Union{Array{Float32},CuArray{Float32}}
     stack::Union{Array{Float32},CuArray{Float32}}
 end
 
 struct VMSuperStates
-    current_instructions::Union{Array{Float32},CuArray{Float32}}
-    top_of_stacks::Union{Array{Float32},CuArray{Float32}}
+    instructionpointers::Union{Array{Float32},CuArray{Float32}}
+    stackpointers::Union{Array{Float32},CuArray{Float32}}
     stacks::Union{Array{Float32},CuArray{Float32}} # num instructions x stack
 end
 # TODO just have an array of structs? maybe easier to make mutable, instructions can directly modify 
 
 
-# Zygote.@adjoint VMSuperStates(x,y,z) = VMSuperStates(x,y,z), di -> (di.current_instructions, di.top_of_stacks, di.stacks)
-# Zygote.@adjoint VMState(x,y,z) = VMState(x,y,z), di -> (di.current_instruction, di.top_of_stack, di.stack)
-a::Number * b::VMState = VMState(a * b.current_instruction, a * b.top_of_stack, a * b.stack)
+# Zygote.@adjoint VMSuperStates(x,y,z) = VMSuperStates(x,y,z), di -> (di.instructionpointers, di.stackpointers, di.stacks)
+# Zygote.@adjoint VMState(x,y,z) = VMState(x,y,z), di -> (di.instructionpointer, di.stackpointer, di.stack)
+a::Number * b::VMState = VMState(a * b.instructionpointer, a * b.stackpointer, a * b.stack)
 a::VMState * b::Number = b * a
-a::VMState + b::VMState = VMState(a.current_instruction + b.current_instruction, a.top_of_stack + b.top_of_stack, a.stack + b.stack)
-a::VMState - b::VMState = VMState(a.current_instruction - b.current_instruction, a.top_of_stack - b.top_of_stack, a.stack - b.stack)
+a::VMState + b::VMState = VMState(a.instructionpointer + b.instructionpointer, a.stackpointer + b.stackpointer, a.stack + b.stack)
+a::VMState - b::VMState = VMState(a.instructionpointer - b.instructionpointer, a.stackpointer - b.stackpointer, a.stack - b.stack)
 
-length(a::VMSuperStates) = size(a.current_instructions)[3]
-a::Union{Array,CuArray} * b::VMSuperStates = VMSuperStates(a .* b.current_instructions, a .* b.top_of_stacks, a .* b.stacks)
+length(a::VMSuperStates) = size(a.instructionpointers)[3]
+a::Union{Array,CuArray} * b::VMSuperStates = VMSuperStates(a .* b.instructionpointers, a .* b.stackpointers, a .* b.stacks)
 a::VMSuperStates * b::Union{Array,CuArray} = b * a
 
 function super_step(state::VMState, program, instructions)
     # TODO instead of taking a state, take the separate arrays as args? Since CuArray doesn't like Structs
     # TODO batch the individual array (eg add superpose dimension -- can that be a struct or needs to be separate?)
     newstates = [instruction(state) for instruction in instructions]
-    current_instructions = cat([x.current_instruction for x in newstates]..., dims=3)
-    top_of_stacks = cat([x.top_of_stack for x in newstates]..., dims=3)
+    instructionpointers = cat([x.instructionpointer for x in newstates]..., dims=3)
+    stackpointers = cat([x.stackpointer for x in newstates]..., dims=3)
 
     stacks = cat([x.stack for x in newstates]..., dims=3)
 
     states = VMSuperStates(
-        current_instructions,
-        top_of_stacks,
+        instructionpointers,
+        stackpointers,
         stacks,
     )
-    current = program .* state.current_instruction'
+    current = program .* state.instructionpointer'
     summed = sum(current, dims=2) 
     summed = reshape(summed, (1, 1, :))
     scaledstates = summed * states 
     reduced = VMState( 
-        sum(scaledstates.current_instructions, dims=3)[:,:,1],
-        sum(scaledstates.top_of_stacks, dims=3)[:,:,1],
+        sum(scaledstates.instructionpointers, dims=3)[:,:,1],
+        sum(scaledstates.stackpointers, dims=3)[:,:,1],
         sum(scaledstates.stacks, dims=3)[:,:,1],
     )
     normit(reduced)
@@ -83,13 +83,13 @@ function instr_dup(state::VMState)
     #= 
     DUP Should duplicate top of stack, and push to top of stack 
     =#
-    new_top_of_stack = roll(state.top_of_stack, -1)
-    new_stack = state.stack .* (1.f0 .- state.top_of_stack') .+ state.stack .* new_top_of_stack'
-    new_current_instruction = roll(state.current_instruction, 1)
+    new_stackpointer = roll(state.stackpointer, -1)
+    new_stack = state.stack .* (1.f0 .- state.stackpointer') .+ state.stack .* new_stackpointer'
+    new_instructionpointer = roll(state.instructionpointer, 1)
 
     VMState(
-        new_current_instruction,
-        new_top_of_stack,
+        new_instructionpointer,
+        new_stackpointer,
         new_stack,
     )
     
@@ -99,13 +99,13 @@ function instr_swap(state::VMState)
     #= 
     SWAP Should swap top of stack with second to top of stack
     =#
-    new_top_of_stack = roll(state.top_of_stack, -1)
-    new_stack = state.stack .* (1.f0 .- state.top_of_stack') .+ state.stack .* new_top_of_stack'
-    new_current_instruction = roll(state.current_instruction, 1)
+    new_stackpointer = roll(state.stackpointer, -1)
+    new_stack = state.stack .* (1.f0 .- state.stackpointer') .+ state.stack .* new_stackpointer'
+    new_instructionpointer = roll(state.instructionpointer, 1)
 
     VMState(
-        new_current_instruction,
-        new_top_of_stack,
+        new_instructionpointer,
+        new_stackpointer,
         new_stack,
     )
     
@@ -122,19 +122,19 @@ function instr_gotoifnotzerofull(zerovec, nonintvalues, state::VMState)
     Apply top of stack prob? =#
 
     # TODO don't recalc 
-    stackscaled = state.stack .* state.top_of_stack'
+    stackscaled = state.stack .* state.stackpointer'
     probofgoto = 1 .- sum(stackscaled .* zerovec, dims=1)
-    firstpoptop = roll(state.top_of_stack, 1)
+    firstpoptop = roll(state.stackpointer, 1)
     secondstackscaled = state.stack .* firstpoptop' 
     jumpvalprobs = sum(secondstackscaled, dims=2)
 
     # Assume ints start at 0 should skip 0?
     # Constraint -- length(intvalues) > length of current instruction ?
     jumpvalprobs = jumpvalprobs[length(nonintvalues) + 1:end]
-    # TODO length(current_instruction) compare and truncate
+    # TODO length(instructionpointer) compare and truncate
     jumpvalprobs = jumpvalprobs[1:end]
-    currentinstructionforward = (1.f0 - sum(jumpvalprobs)) * roll(state.current_instruction, 1)
-    new_current_instruction = currentinstructionforward .+ jumpvalprobs[1:length(state.current_instruction)]
+    currentinstructionforward = (1.f0 - sum(jumpvalprobs)) * roll(state.instructionpointer, 1)
+    new_instructionpointer = currentinstructionforward .+ jumpvalprobs[1:length(state.instructionpointer)]
     newtop = roll(firstpoptop, 1)
 
     # jumpvalprobs[:end]
@@ -142,10 +142,10 @@ function instr_gotoifnotzerofull(zerovec, nonintvalues, state::VMState)
     # for each value 0-max in stack, 
     # (??sum diagonal of equivalent jump locations. eg 0 at x is equivalent to 1 at x+1 on stack)
     # TODO if not int value, set to roll 1 forward?
-    # TODO add jumpvalprobs to current_instruction? Then normalize?
+    # TODO add jumpvalprobs to instructionpointer? Then normalize?
 
     VMState(
-        new_current_instruction,
+        new_instructionpointer,
         newtop,
         state.stack,
     )
@@ -164,18 +164,18 @@ function instr_val(valhotvec, state::VMState)
     # Preallocate intermediate arrays? 1 intermediate state for each possible command, so not bad to allocate ahead of time
     # sizehint
     # set return type to force allocation
-    # display(state.top_of_stack)
-    new_top_of_stack = roll(state.top_of_stack, -1)
-    new_current_instruction = roll(state.current_instruction, 1)
+    # display(state.stackpointer)
+    new_stackpointer = roll(state.stackpointer, -1)
+    new_instructionpointer = roll(state.instructionpointer, 1)
     # display(valhotvec)
-    # display(new_top_of_stack)
-    topscaled = valhotvec * new_top_of_stack'
-    stackscaled = state.stack .* (1.f0 .- new_top_of_stack')
-    # new_stack = state.stack .* (1.f0 .- new_top_of_stack') .+ valhotvec * new_top_of_stack'
+    # display(new_stackpointer)
+    topscaled = valhotvec * new_stackpointer'
+    stackscaled = state.stack .* (1.f0 .- new_stackpointer')
+    # new_stack = state.stack .* (1.f0 .- new_stackpointer') .+ valhotvec * new_stackpointer'
     new_stack = stackscaled .+ topscaled
     VMState(
-        new_current_instruction,
-        new_top_of_stack,
+        new_instructionpointer,
+        new_stackpointer,
         new_stack,
     )
 end
@@ -202,8 +202,8 @@ function roll(a::Union{CuArray,Array}, increment)
 end
 
 function check_state_asserts(state)
-    @assert isapprox(sum(state.current_instruction), 1.0)
-    @assert isapprox(sum(state.top_of_stack), 1.0)
+    @assert isapprox(sum(state.instructionpointer), 1.0)
+    @assert isapprox(sum(state.stackpointer), 1.0)
     for col in eachcol(state.stack)
         @assert isapprox(sum(col), 1.0)
     end
@@ -211,8 +211,8 @@ end
 
 function assert_no_nans(state::VMState)
     @assert !any(isnan.(state.stack)) ## Damn, putting an assert removes the NaN
-    @assert !any(isnan.(state.top_of_stack)) ## Damn, putting an assert removes the NaN
-    @assert !any(isnan.(state.current_instruction)) ## Damn, putting an assert removes the NaN
+    @assert !any(isnan.(state.stackpointer)) ## Damn, putting an assert removes the NaN
+    @assert !any(isnan.(state.instructionpointer)) ## Damn, putting an assert removes the NaN
 end
 
 function softmaxmask(mask, prog)
@@ -229,8 +229,8 @@ end
 
 function normit(a::VMState; dims=1)
     VMState(
-        normit(a.current_instruction, dims=dims),
-        normit(a.top_of_stack, dims=dims),
+        normit(a.instructionpointer, dims=dims),
+        normit(a.stackpointer, dims=dims),
         normit(a.stack, dims=dims),
     )
 end
@@ -267,15 +267,15 @@ end
 
 function init_state(stackdepth, programlen, allvalues)
     stack = zeros(Float32, length(allvalues), stackdepth)
-    current_instruction = zeros(Float32, programlen, )
-    top_of_stack = zeros(Float32, stackdepth, )
+    instructionpointer = zeros(Float32, programlen, )
+    stackpointer = zeros(Float32, stackdepth, )
     stack[1,:] .= 1.f0
-    current_instruction[1] = 1.f0
-    top_of_stack[1] = 1.f0
+    instructionpointer[1] = 1.f0
+    stackpointer[1] = 1.f0
     # @assert isbitstype(stack) == true
     state = VMState(
-        current_instruction |> device,
-        top_of_stack |> device,
+        instructionpointer |> device,
+        stackpointer |> device,
         stack |> device,
     )
     state
@@ -321,11 +321,11 @@ function run(state, program, instructions, ticks)
 end
 
 function loss(ŷ, y)
-    # TODO top of stack super position actual stack. add tiny amount of current_instruction just because
+    # TODO top of stack super position actual stack. add tiny amount of instructionpointer just because
     # Technically current instruction doesn't really matter
     crossentropy(ŷ.stack, y.stack) +
-    crossentropy(ŷ.top_of_stack, y.top_of_stack) +
-    crossentropy(ŷ.current_instruction, y.current_instruction)
+    crossentropy(ŷ.stackpointer, y.stackpointer) +
+    crossentropy(ŷ.instructionpointer, y.instructionpointer)
 end
 
 function accuracy(hidden, target, trainmask)
--- tests.jl
+++ tests.jl
@@ -31,58 +31,19 @@ end
 
 function init_random_state(stackdepth, programlen, allvalues)
     stack = rand(Float32, length(allvalues), stackdepth)
-    current_instruction = zeros(Float32, programlen, )
-    top_of_stack = zeros(Float32, stackdepth, )
-    #current_instruction = rand(Float32, programlen, )
-    #top_of_stack = rand(Float32, stackdepth, )
-    current_instruction[1] = 1.f0
-    top_of_stack[1] = 1.f0
+    instructionpointer = zeros(Float32, programlen, )
+    stackpointer = zeros(Float32, stackdepth, )
+    #instructionpointer = rand(Float32, programlen, )
+    #stackpointer = rand(Float32, stackdepth, )
+    instructionpointer[1] = 1.f0
+    stackpointer[1] = 1.f0
     state = VMState(
-        current_instruction |> device,
-        top_of_stack |> device,
+        instructionpointer |> device,
+        stackpointer |> device,
         stack |> device,
     )
     normit(state)
 end
-#=
-function test_instr_dup()
-    intvalues = [i for i in 0:args.maxint]
-    nonintvalues = ["blank"]
-    allvalues = [nonintvalues; intvalues]
-
-    instr_gotoifnotzero = partial(instr_gotoifnotzerofull, valhot(0, allvalues), nonintvalues)
-
-    val_instructions = [partial(instr_val, valhot(i, allvalues)) for i in intvalues]
-    instructions = [[instr_gotoifnotzero, instr_dup]; val_instructions]
-    num_instructions = length(instructions)
-
-    discrete_program = create_random_discrete_program(args.programlen, instructions)
-    target_program = convert(Array{Float32}, onehotbatch(discrete_program, instructions))
-    trainmask = create_trainable_mask(args.programlen, args.inputlen)
-    hiddenprogram = deepcopy(target_program)
-    hiddenprogram[:, trainmask] = glorot_uniform(size(hiddenprogram[:, trainmask]))
-
-
-    # Initialize
-
-    trainmaskfull = repeat(trainmask', outer=(size(hiddenprogram)[1], 1)) |> device
-    softmaxprog = partial(softmaxmask, trainmaskfull |> device)
-    applyfullmaskprog = partial(applyfullmask, trainmaskfull)
-    applyfullmasktohidden = partial((mask, prog) -> mask .* prog, trainmaskfull)
-
-    hiddenprogram = hiddenprogram |> device
-    program = softmaxprog(hiddenprogram) |> device
-    target_program = target_program |> device
-    hiddenprogram = hiddenprogram |> device
-    trainmask = trainmask |> device
-
-
-    blank_state = init_state(args.stackdepth, args.programlen, allvalues)
-    blank_state_random = init_random_state(args.stackdepth, args.programlen, allvalues)
-
-    instr_dup(blank_state)
-end
-=#
 
 blank_state = init_state(3, 4, [i for i in 0:3])
 blank_state_random = init_random_state(3, 4, [i for i in 0:3])
@@ -90,5 +51,3 @@ check_state_asserts(blank_state)
 check_state_asserts(blank_state_random)
 
 newstate = instr_dup(blank_state_random)
-
-#new_state = test_instr_dup()
